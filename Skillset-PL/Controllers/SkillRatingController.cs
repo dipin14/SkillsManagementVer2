ff8b17a75da256dd64c3573198251d0dc24c75a8
rizwan.ebrahim@experionglobal.com <rizwan.ebrahim@experionglobal.com>
2018-03-23 15:58:21 +0530

    PARTIAL IMPLEMENTED Skill Rating

---
+++ Skillset-PL/Controllers/SkillRatingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Skillset_BLL.Services;
+using Skillset_PL.ViewModelExtensions;
+using Skillset_PL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Skillset_PL.Controllers
+{
+    public class SkillRatingController : Controller
+    {
+
+        private readonly ISkillService _skillService;
+
+        public SkillRatingController(ISkillService skillService)
+        {
+            _skillService = skillService;
+        }
+
+        // GET: Skill
+        public ActionResult GetAllSkills()
+        {
+
+            return View();
+        }
+
+        public ActionResult EmployeeRating()
+        {
+            var skillList = _skillService.GetAllSkills().ToViewModelList();
+            return View( skillList);
+        }
+
+    }
+}
---
+++ Skillset-PL/Scripts/SkillRating.js
@@ -0,0 +1,51 @@
+﻿
+function CRate(id, val) {
+
+    document.getElementById("Rating " + id).value = val;
+
+    for (var i = 1; i <= val; i++) {
+
+        var element = document.getElementById("Rate" + i + " " + id);
+        element.className = 'starGlow';
+    }
+    // unselect remaining
+    for (var i = val + 1; i <= 5; i++) {
+
+        var element = document.getElementById("Rate" + i + " " + id);
+        element.className = 'starFade';
+    }
+}
+
+function CRateOver(id, val) {
+
+    for (var i = 1; i <= val; i++) {
+
+        var element = document.getElementById("Rate" + i + " " + id);
+        element.className = 'starGlow';
+    }
+}
+
+function CRateOut(id, val) {
+    for (var i = 1; i <= val; i++) {
+
+        var element = document.getElementById("Rate" + i + " " + id);
+        element.className = 'starFade';
+    }
+}
+
+function CRateSelected(id) {
+
+    var setRating = document.getElementById("Rating " + id).value
+
+    for (var i = 1; i <= setRating; i++) {
+
+        var element = document.getElementById("Rate" + i + " " + id);
+        element.className = 'starGlow';
+    }
+}
+
+function SubmitRating( data)
+{
+    //for(var i=0;i<)
+    alert(data);
+}
