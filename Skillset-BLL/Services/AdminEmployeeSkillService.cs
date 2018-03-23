68c0d77bfb298a83ef81107e08b77f81200811f8
Nikhal62 <nikhal.p@experionglobal.com>
2018-03-23 17:03:13 +0530

    Admin view of employees skills

---
+++ Skillset-BLL/Services/AdminEmployeeSkillService.cs
@@ -0,0 +1,67 @@
+﻿using Common.DTO;
+using Skillset_DAL.Models;
+using Skillset_DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skillset_BLL.Services
+{
+    public class AdminEmployeeSkillService : IAdminEmployeeSkillService
+    {
+        private readonly IAdminEmployeeSkillRepository _iEmpSkillRepository;
+
+        //DI to UserRepository
+        public AdminEmployeeSkillService(IAdminEmployeeSkillRepository empSkillRepository)
+        {
+            _iEmpSkillRepository = empSkillRepository;
+        }
+
+        public IEnumerable<AdminEmployeeDTO> ViewSearchedRecords(string option, string searchKey)
+        {
+            List<Employee> employeeList = _iEmpSkillRepository.GetEmployeeDetails(option, searchKey);
+            List<string> designationList = new List<string>();
+            foreach (var emp in employeeList)
+            {
+                string designation = _iEmpSkillRepository.FindDesignation(emp.DesignationId);
+                designationList.Add(designation);
+            }
+            List<AdminEmployeeDTO> employeeDetailsList = new List<AdminEmployeeDTO>();
+            for (int loopVar = 0; loopVar < employeeList.Count; loopVar++)
+            {
+                AdminEmployeeDTO emp = new AdminEmployeeDTO();
+                emp.EmployeeCode = employeeList[loopVar].EmployeeCode;
+                emp.Name = employeeList[loopVar].Name;
+                emp.Designation = designationList[loopVar];
+                employeeDetailsList.Add(emp);
+            }
+            return employeeDetailsList;
+        }
+
+        public IEnumerable<AdminSkillDTO> GetSkillDetails(string employeeCode)
+        {
+            List<SkillRating> skillList = _iEmpSkillRepository.GetSkillDetails(employeeCode);
+            List<string> skillNameList = new List<string>();
+            List<int> skillValueList = new List<int>();
+            foreach (var item in skillList)
+            {
+                string skillName = _iEmpSkillRepository.FindSkillName(item.SkillId);
+                skillNameList.Add(skillName);
+                int skillValue = _iEmpSkillRepository.FindSkillValue(item.RatingId);
+                skillValueList.Add(skillValue);
+            }
+            List<AdminSkillDTO> skillDetailsList = new List<AdminSkillDTO>();
+            for (int loopVar = 0; loopVar < skillList.Count; loopVar++)
+            {
+                AdminSkillDTO skill = new AdminSkillDTO();
+                skill.SkillName = skillNameList[loopVar];
+                skill.SkillValue = skillValueList[loopVar];
+                skill.RatingDate = skillList[loopVar].RatingDate;
+                skillDetailsList.Add(skill);
+            }
+            return skillDetailsList;
+        }
+    }
+}
--- Skillset-DAL/Models/Rating.cs
+++ Skillset-DAL/Models/Rating.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace Skillset_DAL.Models
 {
-    public class Role
+    public class Rating
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public int Value { get; set; }
+        public string Note { get; set; }
     }
 }
