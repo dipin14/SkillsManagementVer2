6dcfb97b4926b05c392ded5a2238230be51ddd5b
rizwan.ebrahim@experionglobal.com <rizwan.ebrahim@experionglobal.com>
2018-03-23 18:05:27 +0530

    styles IMPLEMENTED

--- Skillset-PL/Scripts/SkillRating.js
+++ Skillset-PL/Scripts/SkillRating.js
@@ -49,3 +49,21 @@ function SubmitRating( data)
     //for(var i=0;i<)
     alert(data);
 }
+/*collapsing div*/
+$(document).on('click', '.panel-heading span.clickable', function (e) {
+    var $this = $(this);
+    if (!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+    }
+})
+function collapse()
+{ alert('collapse')
+
+$('#viewdetails').collapse('hide')
+}
