b40525d6761d1a1de2baf6d85864e0281024232c
reshmapradeep47@gmail.com <reshmapradeep47@gmail.com>
2018-03-27 14:54:57 +0530

    Changed dbcontext

--- Skillset-DAL/ContextClass/SkillsetDbContext.cs
+++ Skillset-DAL/ContextClass/SkillsetDbContext.cs
@@ -20,7 +20,7 @@ namespace Skillset_DAL.ContextClass
 
             //Fluent API to be written here
 
-
+            modelBuilder.Entity<Skill>().Property(c => c.SkillName).HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute("SK_Name") { IsUnique = true }));
 
 
         }
