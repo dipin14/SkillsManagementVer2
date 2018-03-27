65ed99880afc6787a4786ac3530df31b7314beed
GokulVijayan <gokulpvijayan@gmail.com>
2018-03-26 18:13:44 -0700

    new changes

--- Skillset-DAL/Models/SkillRating.cs
+++ Skillset-DAL/Models/SkillRating.cs
@@ -8,7 +8,10 @@ namespace Skillset_DAL.Models
 {
     public class SkillRating
     {
-        public int Id { get; set; }        public int EmployeeId { get; set; }        public int SkillId { get; set; }        public int RatingId { get; set; }
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public int SkillId { get; set; }
+        public int RatingId { get; set; }
         public DateTime RatingDate { get; set; }
         public string Note { get; set; }
         public bool IsSpecialSkill { get; set; }
