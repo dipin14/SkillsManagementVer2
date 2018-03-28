d912ee468c91f8b9a1e2551a458f37469db5199f
SnehaSaajan <snehasaajant@gmail.com>
2018-03-28 11:46:11 +0530

    validations

--- Skillset-PL/Scripts/Employee.js
+++ Skillset-PL/Scripts/Employee.js
@@ -5,7 +5,8 @@
     var dateformat = Date.parse(selectdate);
     var timeDiff = Math.abs(year - dateformat);
     var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24 * 365));
-    if (diffDays < 18)
-        alert("enter a valid birth date");
+    if (diffDays < 18) {
+        document.getElementById("birthvalidation").innerText="Enter a valid birth date";       
+    }
     
 }
