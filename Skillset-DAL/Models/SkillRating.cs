b239912e0ad47c0679439b2a398f9f9a1b88a5c3
Dipin Dinesh <37202091+dipin14@users.noreply.github.com>
2018-03-28 10:24:48 +0530

    Updated SkillRating

--- Skillset-DAL/Models/SkillRating.cs
+++ Skillset-DAL/Models/SkillRating.cs
@@ -9,9 +9,9 @@ namespace Skillset_DAL.Models
     public class SkillRating
     {
         public int Id { get; set; }        
-		public int EmployeeId { get; set; }        
-		public int SkillId { get; set; }        
-		public int RatingId { get; set; }
+	public int EmployeeId { get; set; }        
+	public int SkillId { get; set; }        
+	public int RatingId { get; set; }
         public DateTime RatingDate { get; set; }
         public string Note { get; set; }
         public bool IsSpecialSkill { get; set; }
