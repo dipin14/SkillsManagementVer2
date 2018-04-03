47f83e01e8cfd7987cc2159dd9a5576313c4ce20
Nikhal62 <nikhal.p@experionglobal.com>
2018-04-03 15:18:25 +0530

    latest star

--- Skillset-DAL/Repositories/IAdminEmployeeSkillRepository.cs
+++ Skillset-DAL/Repositories/IAdminEmployeeSkillRepository.cs
@@ -15,7 +15,7 @@ namespace Skillset_DAL.Repositories
         /// <param name="option"></param>
         /// <param name="searchKey"></param>
         /// <returns></returns>
-        List<Employee> GetEmployeeDetails(string option, string searchKey);
+        List<Employee> GetEmployeeDetails(string searchKey);
         
         /// <summary>
         /// Finding designation from table Designation
