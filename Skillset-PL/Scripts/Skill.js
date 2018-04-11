0fa4980fd54d889bcbaab2c0d711f1c2614347e3
reshmapradeep47@gmail.com <reshmapradeep47@gmail.com>
2018-04-11 16:09:48 +0530

    sidenav border persisting

--- Skillset-PL/Scripts/Skill.js
+++ Skillset-PL/Scripts/Skill.js
@@ -3,3 +3,7 @@
     $(".has-error").empty();
     $(".text-danger").empty();
 }
+
+$(document).ready(function () {
+    $("ul li:eq(2) a").parent().addClass('active').parent().parent().addClass('active');
+});
